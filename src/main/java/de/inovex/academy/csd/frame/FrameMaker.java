9d8eaa1bfae708f792225e4de117dc21f00ffc22
danielksan81 <danielksan81@gmail.com>
2014-05-07 14:05:46 +0200

    Fixed FrameMaker regarding new tests to handle number of lines.

--- src/main/java/de/inovex/academy/csd/frame/FrameMaker.java
+++ src/main/java/de/inovex/academy/csd/frame/FrameMaker.java
@@ -1,22 +1,25 @@
 package de.inovex.academy.csd.frame;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FrameMaker {
 
 	public List<String> makeFrame(List<String> stringList) {
 		if (null != stringList) {
-			
+			List<String> result = new ArrayList<>();
 			String ersterEintrag = stringList.get(0);
 			int length = ersterEintrag.length() + 2;
 			StringBuffer sb = new StringBuffer();
 			for (int i = 0; i < length; i++) {
 				sb.append("*");
 			}
-			return Arrays.asList(sb.toString(), "*" + ersterEintrag + "*",
-					sb.toString());
+			result.add(sb.toString());
+			for (String stringElement : stringList) {
+				result.add("*"+stringElement+"*");
+			}
+			result.add(sb.toString());
+			return result;
 		} else {
 			return null;
 		}
